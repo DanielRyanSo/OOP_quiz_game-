c7ec7fb521064c8aa8a15966c2bc254540a337dd
Jeric Tan <thejerictan@gmail.com>
2025-11-30 13:08:36 +0800

    Daily quiz (Brain Boost) implementation

--- OOP_Proj/Pages/Index.cshtml.cs
+++ OOP_Proj/Pages/Index.cshtml.cs
@@ -1,12 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using GameAuth.Data;
+using GameAuth.Models;
+using System;
+using System.Linq;
 
 namespace OOP_Proj.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly AppDbContext _db;
+        public IndexModel(AppDbContext db)
+        {
+            _db = db;
+        }
         public string Username { get; set; } = "";
-
+        public bool DailyCompleted { get; set; }
         public IActionResult OnGet()
         {
             var username = HttpContext.Session.GetString("Username");
@@ -14,8 +23,10 @@ namespace OOP_Proj.Pages
             {
                 return RedirectToPage("/Login");
             }
-
             Username = username;
+            var dayKey = DateTime.UtcNow.AddHours(8).ToString("yyyy-MM-dd");
+            DailyCompleted = _db.DailyCompletions
+            .Any(c => c.Username == username && c.DayKey == dayKey && c.Completed);
             return Page();
         }
 
